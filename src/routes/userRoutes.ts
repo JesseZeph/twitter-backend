622a7067cc4ca4bba0cfd18f29a035e1eea4a26e
Jesse Josiah <jessezephyr@gmail.com>
2023-10-28 17:19:20 +0100

    Setup CRUD endpoints for user and tweet

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+
+const router = Router()
+
+router.post("/", (req, res) => {
+    res.status(501).json({error: "Not implemented"})
+})
+// list users
+router.get("/", (req, res) => {
+    res.status(501).json({error: "Not implemented"})
+})
+
+//get one user
+router.get("/:id", (req, res) => {
+    const {id} = req.params;
+    res.status(501).json({error: "Not implemented"})
+})
+
+
+//update user
+router.put("/:id", (req, res) => {
+    const {id} = req.params;
+    res.status(501).json({error: "Not implemented"})
+})
+
+//delete user
+router.delete("/:id", (req, res) => {
+    const {id} = req.params;
+    res.status(501).json({error: "Not implemented"})
+})
+export default router;
